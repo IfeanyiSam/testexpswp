dd626372142da33fe1cc5a6ea1825f7a345306a3
Samuel Nnanna <samuelnnanna71@gmail.com>
2024-08-22 21:14:21 +0100

    opened port 22 on servers

--- node-app/main.tf
+++ node-app/main.tf
@@ -69,6 +69,14 @@ resource "aws_security_group" "app_web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
